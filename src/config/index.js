cb5de92b1756fc759f0cf50d4364d74a2ff62819
oscar <jincan.liu@gmail.com>
2018-08-16 23:54:05 +0800

    config url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     port : '3000',
-    service_api : 'http://api.eth.local.cn',
+    service_api : 'http://47.74.184.57',
     token_address : '0xed494c9e2f8e34e53bdd0ea9b4d80305cb15c5c2', //代币合约地址
 };
 
